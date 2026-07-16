27fc82460fba92f1638d6d086b47bd06d82f28c8
Vijay-V <vijay.v@mitrahsoft.io>
2026-07-16 19:06:03 +0530

    Conflict in global Ecception handler resolved

--- src/main/java/com/mitrahsoft/company_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mitrahsoft/company_management/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mitrahsoft.company_management.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
@@ -18,21 +19,26 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValid(MethodArgumentNotValidException e){
-        Map<String,String> errors = new HashMap<>();
-        e.getBindingResult().getFieldErrors().forEach(error-> errors.put(error.getField(),error.getDefaultMessage()));
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(value = NoSuchElementException.class)
-    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e){
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
-        @ExceptionHandler(OfficialDetailsAlreadyExistException.class)
-        public ResponseEntity<String> handleOfficialDetailsAlreadyExist(
-                OfficialDetailsAlreadyExistException ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    @ExceptionHandler(value = EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(OfficialDetailsAlreadyExistException.class)
+    public ResponseEntity<String> handleOfficialDetailsAlreadyExist(
+            OfficialDetailsAlreadyExistException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
 }
