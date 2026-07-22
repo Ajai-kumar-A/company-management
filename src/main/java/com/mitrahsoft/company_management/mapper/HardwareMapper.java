9507df5ebf09891ce892f4e0b9fbc5dca85da2d5
SuriyaBhuvanesh-E <suriyabhuvanesh.e@mitrahsoft.io>
2026-07-22 11:53:30 +0530

    Resolved the Conflicts and Merged to dev

--- src/main/java/com/mitrahsoft/company_management/mapper/HardwareMapper.java
+++ src/main/java/com/mitrahsoft/company_management/mapper/HardwareMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface HardwareMapper {
 
     Hardware toEntity(HardwareRequestDto HardwareDto);
-    @Mapping(target = "employeeId",source = "employee.employeeId")
+    @Mapping(target = "employee")
     HardwareResponseDto toDto(Hardware hardware);
 
     List<HardwareListResDto> toDtoList(List<Hardware> hardwareList);
--- src/main/java/com/mitrahsoft/company_management/mapper/PersonalDetailsMapper.java
+++ src/main/java/com/mitrahsoft/company_management/mapper/PersonalDetailsMapper.java
@@ -1,15 +1,17 @@
 package com.mitrahsoft.company_management.mapper;
 
+import com.mitrahsoft.company_management.dto.EmployeeDto.EmployeeRevResDto;
 import com.mitrahsoft.company_management.dto.PersonalDetailsDto.PersonalDetailsListResDto;
 import com.mitrahsoft.company_management.dto.PersonalDetailsDto.PersonalDetailsRequestDto;
 import com.mitrahsoft.company_management.dto.PersonalDetailsDto.PersonalDetailsResponseDto;
+import com.mitrahsoft.company_management.entity.Employee;
 import com.mitrahsoft.company_management.entity.PersonalDetails;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {EmployeeMapper.class})
+@Mapper(componentModel = "spring")
 public interface PersonalDetailsMapper {
     PersonalDetails toEntity(PersonalDetailsRequestDto personalDetailsRequestDto);
     @Mapping(target = "employee")
@@ -17,4 +19,7 @@ public interface PersonalDetailsMapper {
 
     List<PersonalDetailsListResDto> toDtoList(List<PersonalDetails> personalDetailsList);
 
+    @Mapping(target = "branchId", source = "branch.branchId")
+    @Mapping(target = "stackId", source = "techStack.id")
+    EmployeeRevResDto toEmployeeDto(Employee employee);
 }
